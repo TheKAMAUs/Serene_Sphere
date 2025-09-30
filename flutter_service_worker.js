7ca87b312e7d1b0aebe478be9bcc44b13a372ddf
TheKAMAUs <hewsmohamed@gmail.com>
2025-09-30 22:20:45 +0300

    Built web
    Branch: main
    Commit: 2050ff5cc6be70346e96b7e367d7f2ebf6dde7ef (dirty)
    
    package:peanut 6.0.0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -42,7 +42,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "61f9c0d5ba06966d1ce559cb7e60a680
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "a85c0c5947afa75c94b023aba13e8c8d",
+"flutter_bootstrap.js": "dacf650d992382452f04c3e4926ec4ec",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
